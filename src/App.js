e6e84452b6c6705cfaf5fcc32800a231623e1227
Jason Matthew <jasonm4130@gmail.com>
2019-12-08 07:24:23 +1000

    setup service worker and styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from 'styled-components';
+
+const AppWrapper = styled.div`
+  text-align: center;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen','Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+`
+
+const Code = styled.code`
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+`
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${AppLogoSpin} infinite 20s linear;
+  }
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppWrapper>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <Code>src/App.js</Code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </AppWrapper>
   );
 }
 
